66d731707a31b1b8b3f11bec14b133e237821e7b
4BigTasty4 <eldanizyolchuyev@gmail.com>
2024-03-24 16:23:12 +0400

    HM1
    HM1

---
+++ HM1/Client/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Net.Sockets;
+using System.Text;
+
+class Client
+{
+    static void Main(string[] args)
+    {
+        StartClient();
+    }
+
+    static void StartClient()
+    {
+        TcpClient client = new TcpClient("127.0.0.1", 8888);
+        Console.WriteLine("Connected to server.");
+        
+        NetworkStream stream = client.GetStream();
+
+        string message;
+
+        do
+        {
+            Console.WriteLine("Client: ");
+            message = Console.ReadLine();
+            byte[] messageBuffer = Encoding.ASCII.GetBytes(message);
+            stream.Write(messageBuffer, 0, messageBuffer.Length);
+
+            byte[] buffer = new byte[1024];
+            int bytesRead = stream.Read(buffer, 0, buffer.Length);
+            string response = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+            Console.WriteLine($"Server: {response}");
+            
+        } while (message.ToLower() != "bye");
+        
+        stream.Close();
+        client.Close();
+        Console.WriteLine("Connection closed.");
+    }
+}
---
+++ HM1/Server/Program.cs
@@ -0,0 +1,52 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class Server
+{
+    static void Main(string[] args)
+    {
+        StartServer();
+    }
+
+    static void StartServer()
+    {
+        TcpListener listener = new TcpListener(IPAddress.Any, 8888);
+        listener.Start();
+        Console.WriteLine("Server started. Waiting for connections...");
+
+        TcpClient client = listener.AcceptTcpClient();
+        Console.WriteLine("Client connected.");
+
+        NetworkStream stream = client.GetStream();
+
+        byte[] buffer = new byte[1024];
+        int bytesRead;
+
+        bool shouldExit = false;
+
+        while (!shouldExit)
+        {
+            bytesRead = stream.Read(buffer, 0, buffer.Length);
+            string dataReceived = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+            Console.WriteLine($"Client: {dataReceived}");
+
+            if (dataReceived.ToLower() == "bye")
+            {
+                Console.WriteLine("Client disconnected.");
+                shouldExit = true;
+            }
+            else
+            {
+                Console.Write("Server: ");
+                string response = Console.ReadLine();
+                byte[] responseBuffer = Encoding.ASCII.GetBytes(response);
+                
+                stream.Write(responseBuffer, 0, responseBuffer.Length);
+            }
+        }
+        stream.Close();
+        client.Close();
+        listener.Stop();
+    }
+}
